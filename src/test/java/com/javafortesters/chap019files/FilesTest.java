8609242462bbfdfb2f09a9da17bc666ef36f17ec
aedwards-cognito <alanna.edwards@cognitoiq.com>
2017-06-29 15:01:54 +0100

    Starting the Files chapter.

--- src/test/java/com/javafortesters/chap019files/FilesTest.java
+++ src/test/java/com/javafortesters/chap019files/FilesTest.java
@@ -1,5 +1,43 @@
 package com.javafortesters.chap019files;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.*;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
 public class FilesTest {
-    //code goes here
+
+    @Test
+    public void createTempFile() throws IOException{
+            File myTempFile = File.createTempFile("cottontail", "theSuffix");
+
+            assertThat(myTempFile.exists(), is(true));
+
+            myTempFile.deleteOnExit();
+    }
+
+    @Test
+    public void writeOutTheRoots(){
+        File[] roots = File.listRoots();
+        Assert.assertTrue(roots.length > 0);
+
+        for(File aFile : roots){
+            System.out.println(aFile.getAbsolutePath());
+        }
+
+    }
+
+    @Test
+    public void createFileWithCustomCode() throws IOException{
+        File aTempFile = new File ("d:/tempJavaForTesters.txt");
+        assertThat(aTempFile.exists(), is(false));
+        aTempFile.createNewFile();
+        assertThat(aTempFile.exists(), is(true));
+        aTempFile.delete();
+        assertThat(aTempFile.exists(), is(false));
+    }
+
 }
