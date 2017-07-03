db14535c99fdfd9ac0200f48fd4a465b6a850e62
aedwards-cognito <alanna.edwards@cognitoiq.com>
2017-07-03 20:44:54 +0100

    Starting the Files chapter.

--- src/test/java/com/javafortesters/chap019files/FilesTest.java
+++ src/test/java/com/javafortesters/chap019files/FilesTest.java
@@ -3,6 +3,7 @@ package com.javafortesters.chap019files;
 import org.junit.Assert;
 import org.junit.Test;
 
+import javax.sound.midi.Soundbank;
 import java.io.*;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -38,6 +39,51 @@ public class FilesTest {
         assertThat(aTempFile.exists(), is(true));
         aTempFile.delete();
         assertThat(aTempFile.exists(), is(false));
+    }
+
+    @Test
+    public void checkTempDirIsDir(){
+        File tempA = new File(System.getProperty("java.io.tmpdir"));
+
+        assertThat(tempA.isDirectory(), is(true));
+        assertThat(tempA.isFile(), is(false));
+    }
+
+    @Test
+    public void writeToPrintWriterThenAppend() throws IOException{
+        File aTempFile = File.createTempFile("printWriterPrint", null);
+
+        System.out.println("First text");
+
+        FileWriter writer = new FileWriter(aTempFile);
+        BufferedWriter  buffer = new BufferedWriter(writer);
+        PrintWriter print = new PrintWriter(buffer);
+
+        print.println("My second bit of text");
+        print.close();
+
+        //appending the file
+        writer = new FileWriter(aTempFile, true);
+        buffer = new BufferedWriter(writer);
+        print = new PrintWriter(buffer);
+        print.println("third bit of text");
+        print.close();
+
+    }
+
+    @Test
+    public void calcFileLength()throws IOException{
+        File aTempFile = File.createTempFile("printWriterPrint", null);
+
+        FileWriter writer = new FileWriter(aTempFile);
+        BufferedWriter  buffer = new BufferedWriter(writer);
+        PrintWriter print = new PrintWriter(buffer);
+
+        print.println("My first bit of text");
+        print.close();
+
+        System.out.println(aTempFile.length());
+
     }
 
 }
