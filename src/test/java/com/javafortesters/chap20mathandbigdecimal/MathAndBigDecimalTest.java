cca0bc10b8d4fd83679e423db3f008e77fc4d439
Alanna Edwards <alannadenise@users.noreply.github.com>
2017-07-20 16:36:22 +0100

    Complete chap20.

--- src/test/java/com/javafortesters/chap20mathandbigdecimal/MathAndBigDecimalTest.java
+++ src/test/java/com/javafortesters/chap20mathandbigdecimal/MathAndBigDecimalTest.java
@@ -1,8 +1,8 @@
 package com.javafortesters.chap20mathandbigdecimal;
 
 import org.junit.Test;
-
 import java.math.BigDecimal;
+import static org.junit.Assert.assertTrue;
 
 public class MathAndBigDecimalTest {
 
@@ -20,6 +20,27 @@ public class MathAndBigDecimalTest {
                 subtract(new BigDecimal(1.73));
 
         System.out.println("Big Decimal maths: " + moneyBigDecimal);
+    }
+
+    @Test
+    public void basicArithmeticwithBigDecimal(){
+        BigDecimal basicArithmetic = new BigDecimal(0).
+                add(new BigDecimal(10)).
+                multiply(new BigDecimal(2)).
+                subtract(new BigDecimal(10)).
+                divide(new BigDecimal(2));
+
+        System.out.println(basicArithmetic);
+    }
+
+    @Test
+    public void compareTenToOne(){
+        assertTrue(BigDecimal.TEN.compareTo( BigDecimal.ONE) > 0);
+        assertTrue(BigDecimal.ONE.compareTo( BigDecimal.TEN) < 0);
+        assertTrue(BigDecimal.TEN.compareTo( BigDecimal.TEN) == 0);
+        assertTrue(BigDecimal.TEN.compareTo( BigDecimal.ONE) != 0);
+        assertTrue(BigDecimal.TEN.compareTo( BigDecimal.ONE) >= 0);
+        assertTrue(BigDecimal.ONE.compareTo( BigDecimal.TEN) <= 0);
     }
 
 }
